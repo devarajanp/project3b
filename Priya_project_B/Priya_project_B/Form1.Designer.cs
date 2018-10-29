9bb82409ff84eb9c860a0fb9d0e5ef615a14ecd4
devarajanp <42757368+devarajanp@users.noreply.github.com>
2018-10-29 12:05:03 -0600

    Add files via upload

---
+++ Priya_project_B/Priya_project_B/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Priya_project_B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.TextBox();
+            this.box3 = new System.Windows.Forms.TextBox();
+            this.box2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(382, 48);
+            this.box1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(76, 20);
+            this.box1.TabIndex = 0;
+            // 
+            // box3
+            // 
+            this.box3.Location = new System.Drawing.Point(382, 165);
+            this.box3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(76, 20);
+            this.box3.TabIndex = 1;
+            this.box3.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // box2
+            // 
+            this.box2.Location = new System.Drawing.Point(382, 98);
+            this.box2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(76, 20);
+            this.box2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "character1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "character 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 181);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "character 3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 249);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox box1;
+        private System.Windows.Forms.TextBox box3;
+        private System.Windows.Forms.TextBox box2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Priya_project_B/Priya_project_B/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Priya_project_B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string a = box1.Text;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = box1.Text;
+            for (int i = 0; i <= a.Length; i++)
+            {
+                a = a.Replace("@", "");
+                a = a.Replace("}", "");
+                a = a.Replace("#", "");
+                a = a.Replace("(", "");
+                a = a.Replace("[", "");
+                a = a.Replace("]", "");
+                a = a.Replace(")", "");
+            }
+            box1.Text = a;
+            string b = box2.Text;
+            if (b.StartsWith("the") || b.StartsWith("they") || b.StartsWith("all") || b.StartsWith("not") || b.StartsWith("no") || b.StartsWith("bye")) ;
+            {
+                MessageBox.Show("The Character 2 cannot start with the or they or all or not or no or bye");
+                box2.Clear();
+                box2.Focus();
+            }
+            string c = box3.Text;
+            if (c.EndsWith("1") || c.EndsWith("2") || c.EndsWith("3") || c.EndsWith("4") || c.EndsWith("5"))
+            {
+                MessageBox.Show("The Character 3 cannot End with a 1 or 2 or 3 or 4 or 5");
+                box3.Clear();
+                box3.Focus();
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
